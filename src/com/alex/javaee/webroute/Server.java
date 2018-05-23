47a1ff5acd1120261ca86ac72bdf87566931f18d
Alex <lisztes.alex@gmail.com>
2018-05-23 12:32:37 +0200

    Rename the main class

--- src/com/alex/javaee/webroute/Server.java
+++ src/com/alex/javaee/webroute/Server.java
@@ -8,7 +8,7 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-public class WebRoute {
+public class Server {
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
